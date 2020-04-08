0731b1b2eb921fbf462b781b90dc96dcfc300d61
Vineet Singh <singhvineet45@gmail.com>
2020-04-08 19:24:52 +0530

    Initial Commit

---
+++ EMI Calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  EMI Calculator
+//
+//  Created by Vineet Singh on 08/04/20.
+//  Copyright © 2020 Indialends. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
